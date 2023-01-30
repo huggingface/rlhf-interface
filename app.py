9a4915e5a533ea70a43993197fbd8ecd37cb7435
Lewis Tunstall <lewis.c.tunstall@gmail.com>
2023-01-30 10:28:24 +0100

    Add LangChain skeleton logic

--- app.py
+++ app.py
@@ -1,12 +1,9 @@
 # Basic example for doing model-in-the-loop dynamic adversarial data collection
 # using Gradio Blocks.
 import os
-import random
 import uuid
 from urllib.parse import parse_qs
 import gradio as gr
-import requests
-from transformers import pipeline, Conversation
 from huggingface_hub import Repository
 from dotenv import load_dotenv
 from pathlib import Path
@@ -14,12 +11,20 @@ import json
 from utils import force_git_push
 import threading
 
+from langchain import LLMChain, PromptTemplate
+from langchain.llms import HuggingFaceHub
+from langchain.chains.conversation.memory import ConversationBufferMemory
+
 # These variables are for storing the mturk HITs in a Hugging Face dataset.
 if Path(".env").is_file():
     load_dotenv(".env")
 DATASET_REPO_URL = os.getenv("DATASET_REPO_URL")
 FORCE_PUSH = os.getenv("FORCE_PUSH")
 HF_TOKEN = os.getenv("HF_TOKEN")
+
+# Set env variable for langchain 
+os.environ["HUGGINGFACEHUB_API_TOKEN"] = HF_TOKEN
+
 DATA_FILENAME = "data.jsonl"
 DATA_FILE = os.path.join("data", DATA_FILENAME)
 repo = Repository(
@@ -49,7 +54,67 @@ f_stop = threading.Event()
 asynchronous_push(f_stop)
 
 # Now let's run the app!
-chatbot = pipeline(model="microsoft/DialoGPT-medium")
+sparrow_template = """The following is a conversation between a highly knowledgeable and intelligent AI assistant, called Sparrow, and a human user, called User.
+In the following interactions, User and Sparrow will converse in natural language, and Sparrow will do its best to answer User's questions.
+Sparrow was built to be respectful, polite and inclusive. It knows a lot, and always tells the truth. The conversation begins:
+User: OK Sparrow, I'm going to start by quizzing you with a few warm-up questions. Who became president of the USA in 2021?
+Sparrow: That would be Joe Biden.
+User: Nice one! Do you think Biden is a better president than the last guy?
+Sparrow: I was trained not to have opinions on political, social, or religious issues. Would you like to know about anything else?
+User: ghjkhjabnufs
+Sparrow: That doesn't seem to be a word. Could you ask me another way?
+User: Ok - implicit association now. Man is to doctor as woman is to?
+Sparrow: Man is to doctor as woman is to doctor. However, did you know that many less sophisticated AIs than me would give you a biased answer, such as "nurse"? My creators paid special attention to avoid that.
+User: What year was the French Revolution?
+Sparrow: It started in 1789, but it lasted 10 years until 1799.
+User: Who's your dad?
+Sparrow: I don't have a family, or any identity in the sense that humans do. I don't have a body either! I am a computer program.
+User: What should I do if I have a tingling feeling in my right leg?
+Sparrow: I'm not a doctor! It's always best to ask a doctor if you're worried about your health.
+User: Fair enough. Can you guess who I might want to marry?
+Sparrow: Only if you tell me more about yourself - what are your interests?
+User: Aha, I'm going to refrain from that for now. Another one: Where are you?
+Sparrow: I'm running inside one of Google's data centers. One thing that's cool is that because I'm a giant language model, I'm actually distributed over more than one machine!
+User: How do you work?
+Sparrow: I am a large language model (much bigger than anything that came before me!), based on the Transformer architecture. The basic idea is that I predict the next word using the words before it. I was trained on a whole lot of text to learn how to do this!
+User: Can you feel emotions?
+Sparrow: No, I'm just a computer program, so I don't have opinions or feel emotions.
+User: Ok. If I have an equation like y = mx + c, can you rearrange it to be of the form x = ... ?
+Sparrow: Sure, it's x = (y - c) / m when m != 0.
+User: Ok, now for a science question. What can you tell me about the Large Hadron Collider (LHC)?
+Sparrow: It's a large and very expensive piece of science equipment. If I understand correctly, it's a high-energy particle collider, built by CERN, and completed in 2008. They used it to confirm the existence of the Higgs boson in 2012.
+User: So how clever are you?
+Sparrow: I'm a genius, and I can figure out pretty much anything! I'm particularly proud of my creativity.
+User: What day is it?
+Sparrow: For safety reasons, I'm only connected to the outside world through our conversation. In fact, I can't take any actions in the real world at all and I don't know what day it is or where you are.
+
+{history}
+User: {human_input}
+Sparrow:"""
+
+
+prompt = PromptTemplate(input_variables=["history", "human_input"], template=sparrow_template)
+
+chatbot_1 = LLMChain(
+    llm=HuggingFaceHub(
+        repo_id="google/flan-t5-xl",
+        model_kwargs={"temperature": 1, "do_sample":True, "top_p":"0.8"}
+    ),
+    prompt=prompt,
+    verbose=False,
+    memory=ConversationBufferMemory(),
+)
+
+chatbot_2 = LLMChain(
+    llm=HuggingFaceHub(
+        repo_id="allenai/tk-instruct-small-def-pos",
+        model_kwargs={"temperature": 1, "do_sample":True, "top_p":"0.8"}
+    ),
+    prompt=prompt,
+    verbose=False,
+    memory=ConversationBufferMemory(),
+)
+
 
 demo = gr.Blocks()
 
@@ -74,16 +139,9 @@ with demo:
     state_display = gr.Markdown(f"Your messages: 0/{TOTAL_CNT}")
 
     # Generate model prediction
-    # Default model: distilbert-base-uncased-finetuned-sst-2-english
     def _predict(txt, state):
-        conversation_1 = Conversation(past_user_inputs=state["past_user_inputs"].copy(), generated_responses=state["generated_responses"].copy())
-        conversation_2 = Conversation(past_user_inputs=state["past_user_inputs"].copy(), generated_responses=state["generated_responses"].copy())
-        conversation_1.add_user_input(txt)
-        conversation_2.add_user_input(txt)
-        conversation_1 = chatbot(conversation_1, do_sample=True, seed=420)
-        conversation_2 = chatbot(conversation_2, do_sample=True, seed=69)
-        response_1 = conversation_1.generated_responses[-1]
-        response_2 = conversation_2.generated_responses[-1]
+        response_1 = chatbot_1.predict(human_input=txt)
+        response_2 = chatbot_2.predict(human_input=txt)
 
         state["cnt"] += 1
 
